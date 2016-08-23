12fdd5ac8a265ce21a4593e2e455c28752365b85
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-08-23 14:11:19 +0200

    define macro to handle list of options

--- src/config.c
+++ src/config.c
@@ -22,11 +22,12 @@
 #include "ifclib.h"
 #include "simplog.h"
 
-#define CONFIG_PATH   "/etc/camflow.ini"
-#define	LOG_FILE      "/tmp/camflow.clg"
-#define MAX_BRIDGE    32
-#define MAX_OPAQUE    256
-#define MAX_TRACKED   256
+#define CONFIG_PATH       "/etc/camflow.ini"
+#define	LOG_FILE          "/tmp/camflow.clg"
+#define MAX_BRIDGE        32 // arbitrary
+#define MAX_OPAQUE        256 // arbitrary
+#define MAX_TRACKED       256 // arbitrary
+#define MAX_FILTER        32 // filters are 32bits long for now
 
 typedef struct{
   uint32_t machine_id;
@@ -38,7 +39,22 @@ typedef struct{
   int nb_opaque;
   char tracked[MAX_TRACKED][PATH_MAX];
   int nb_tracked;
+  char node_filter[MAX_FILTER][PATH_MAX];
+  int nb_node_filter;
+  char relation_filter[MAX_FILTER][PATH_MAX];
+  int nb_relation_filter;
+  char propagate_node_filter[MAX_FILTER][PATH_MAX];
+  int nb_propagate_node_filter;
+  char propagate_relation_filter[MAX_FILTER][PATH_MAX];
+  int nb_propagate_relation_filter;
 } configuration;
+
+#define ADD_TO_LIST(list, nb, max, error_msg) if(nb+1 >= max){ \
+                                                simplog.writeLog(SIMPLOG_ERROR, error_msg); \
+                                                exit(-1); \
+                                              } \
+                                              strncpy(list[nb], value, PATH_MAX); \
+                                              nb++;
 
 static int handler(void* user, const char* section, const char* name,
                    const char* value)
@@ -62,32 +78,20 @@ static int handler(void* user, const char* section, const char* name,
           pconfig->all = false;
         }
     } else if(MATCH("provenance", "opaque")){
-      if(pconfig->nb_opaque+1 >= MAX_OPAQUE){
-        simplog.writeLog(SIMPLOG_ERROR, "Too many opaque files.");
-        exit(-1);
-      }
-      strncpy(pconfig->opaque[pconfig->nb_opaque], value, PATH_MAX);
-      pconfig->nb_opaque++;
-
+      ADD_TO_LIST(pconfig->opaque, pconfig->nb_opaque, MAX_OPAQUE, "Too many opaque files.");
     } else if(MATCH("provenance", "tracked")){
-      if(pconfig->nb_tracked+1 >= MAX_TRACKED){
-        simplog.writeLog(SIMPLOG_ERROR, "Too many tracked files.");
-        exit(-1);
-      }
-      strncpy(pconfig->tracked[pconfig->nb_tracked], value, PATH_MAX);
-      pconfig->nb_tracked++;
+      ADD_TO_LIST(pconfig->tracked, pconfig->nb_tracked, MAX_TRACKED, "Too many tracked files.");
     }else if(MATCH("ifc", "bridge")){
-      if(pconfig->nb_bridge+1 >= MAX_BRIDGE){
-        simplog.writeLog(SIMPLOG_ERROR, "Too many IFC bridges.");
-        exit(-1);
-      }
-      strncpy(pconfig->bridge[pconfig->nb_bridge], value, PATH_MAX);
-      pconfig->nb_bridge++;
+      ADD_TO_LIST(pconfig->bridge, pconfig->nb_bridge, MAX_BRIDGE, "Too many IFC bridges.");
     } else {
         return 0;  /* unknown section/name, error */
     }
     return 1;
 }
+
+#define LOG_LIST(list, nb, msg) for(i = 0; i < nb; i++){ \
+                                    simplog.writeLog(SIMPLOG_INFO, "%s=%s", msg, list[i]); \
+                                  }
 
 void print_config(configuration* pconfig){
   int i;
@@ -100,12 +104,8 @@ void print_config(configuration* pconfig){
     simplog.writeLog(SIMPLOG_INFO, "Provenance machine_id=%u", pconfig->machine_id);
     simplog.writeLog(SIMPLOG_INFO, "Provenance enabled=%u", pconfig->enabled);
     simplog.writeLog(SIMPLOG_INFO, "Provenance all=%u", pconfig->all);
-    for(i = 0; i < pconfig->nb_opaque; i++){
-      simplog.writeLog(SIMPLOG_INFO, "Provenance opaque=%s", pconfig->opaque[i]);
-    }
-    for(i = 0; i < pconfig->nb_tracked; i++){
-      simplog.writeLog(SIMPLOG_INFO, "Provenance tracked=%s", pconfig->tracked[i]);
-    }
+    LOG_LIST(pconfig->opaque, pconfig->nb_opaque, "Provenance opaque=");
+    LOG_LIST(pconfig->tracked, pconfig->nb_tracked, "Provenance tracked=");
   }
 
   /*
@@ -113,12 +113,17 @@ void print_config(configuration* pconfig){
   */
 
   if(ifc_is_present()){
-    for(i = 0; i < pconfig->nb_bridge; i++){
-      simplog.writeLog(SIMPLOG_INFO, "IFC bridge=%s", pconfig->bridge[i]);
-    }
+    LOG_LIST(pconfig->bridge, pconfig->nb_bridge, "IFC bridge=");
   }
 }
 
+#define APPLY_LIST(list, nb, function, error_msg) for(i = 0; i < nb; i++){ \
+                                                    err = function; \
+                                                    if(err < 0){ \
+                                                      simplog.writeLog(SIMPLOG_ERROR, "%s %s %d", error_msg, list[i], err); \
+                                                      exit(-1); \
+                                                    } \
+                                                  }
 void apply_config(configuration* pconfig){
   int err, i;
   simplog.writeLog(SIMPLOG_INFO, "Applying configuration...");
@@ -145,21 +150,9 @@ void apply_config(configuration* pconfig){
       exit(-1);
     }
 
-    for(i = 0; i < pconfig->nb_opaque; i++){
-      err = provenance_opaque_file(pconfig->opaque[i], true);
-      if(err < 0){
-        simplog.writeLog(SIMPLOG_ERROR, "Error making file opaque %s %d", pconfig->opaque[i], err);
-        exit(-1);
-      }
-    }
+    APPLY_LIST(pconfig->opaque, pconfig->nb_opaque, provenance_opaque_file(pconfig->opaque[i], true), "Error making file opaque");
 
-    for(i = 0; i < pconfig->nb_tracked; i++){
-      err = provenance_track_file(pconfig->tracked[i], true);
-      if(err < 0){
-        simplog.writeLog(SIMPLOG_ERROR, "Error making file tracked %s %d", pconfig->tracked[i], err);
-        exit(-1);
-      }
-    }
+    APPLY_LIST(pconfig->tracked, pconfig->nb_tracked, provenance_opaque_file(pconfig->tracked[i], true), "Error making file tracked");
   }
 
   /*
@@ -167,12 +160,7 @@ void apply_config(configuration* pconfig){
   */
   if(ifc_is_present()){
     simplog.writeLog(SIMPLOG_INFO, "IFC module presence detected.");
-    for(i = 0; i < pconfig->nb_bridge; i++){
-      if(err = ifc_add_bridge(pconfig->bridge[i])){
-        simplog.writeLog(SIMPLOG_ERROR, "Error adding IFC bridge %s %d", pconfig->bridge[i], err);
-        exit(-1);
-      }
-    }
+    APPLY_LIST(pconfig->bridge, pconfig->nb_bridge, ifc_add_bridge(pconfig->bridge[i]), "Error adding IFC bridge");
   }
 }
 
