ed5198ac5ceedebdbd80659f9f7e402591521087
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2019-01-02 10:42:30 +0000

    update copyright for 2019

--- src/camconf.h
+++ src/camconf.h
@@ -1,9 +1,11 @@
 /*
 *
-* Author: Thomas Pasquier <tfjmp2@cam.ac.uk>
+* Author: Thomas Pasquier <thomas.pasquier@bristol.ac.uk>
 *
 * Copyright (C) 2016-2017 University of Cambridge
 * Copyright (C) 2017 Harvard University
+* Copyright (C) 2017-2018 University of Cambridge
+* Copyright (C) 2018-2019 University of Bristol
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License version 2, as
